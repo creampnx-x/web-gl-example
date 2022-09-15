8deec8291af4e65f220c069eaef0d0c0c0abfd66
creampnx_x <2270436024@qq.com>
2022-09-15 22:42:20 +0800

    refactor: render examples fucntion

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
-import { Example } from "./Example"; 
+import renderer from "./renderer";
 import { showError, getWebGLContext } from "./util";
 
+// show examples on page.
+function showExamples(gl, query) {
+    const whichExample = query.get("which") || "clearBackground";
+
+    const render = renderer?.[whichExample];
+    if (!render) throw new Error(`can not find renderer for ${this.whichExample}`);
+    render(gl);
+}
+
+
 function main() {
+    // catch global error and show on the page.
+    window.onerror = showError;
+
     const gl = getWebGLContext("canvas");
 
     if (!gl) throw new Error("can not create webgl context");
-    
-    const query = new URLSearchParams(location.search);
-    const example = new Example(gl, query);
-    example.render();
-}
 
-window.onerror = showError;
+    const query = new URLSearchParams(location.search);
+    showExamples(gl, query);
+}
 
 main();
